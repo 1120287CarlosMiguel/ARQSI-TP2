f695481f6bd16cccfbd1ceda2f1081becd159c3e
1120287CarlosMiguel <1120287@isep.ipp.pt>
2014-11-24 21:44:30 +0000

    Add Model Classes for EntityFramework Code First create DB

---
+++ IDEIMusic/IDEIEditora/IDEIEditora/Models/Album.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace IDEIEditora.Models
+{
+    public class Album
+    {
+        public int AlbumID { get; set; }
+
+        [Required]
+        [StringLength(50, ErrorMessage="Titlhe should 50 characters or less")]
+        public string Title { get; set; }
+
+        [Range(0.1,float.MaxValue)]
+        public float Price { get; set; }
+
+        [DataType(DataType.Url)]
+        public string AlbumArtUrl { get; set; }
+
+        public int GenreID { get; set; }
+
+        public int ArtistID { get; set; }
+
+        public virtual Genre Genre { get; set; }
+
+        public virtual Artist Artist { get; set; }
+
+
+    }
+}
---
+++ IDEIMusic/IDEIEditora/IDEIEditora/Models/Order.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace IDEIEditora.Models
+{
+    public class Order
+    {
+
+        public int OrderID { get; set; }
+
+        [Required]
+        [StringLength(50, ErrorMessage = "First name cannot be longer than 50 characters.")]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(50, ErrorMessage = "Last name cannot be longer than 50 characters.")]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Order Date")]
+        public DateTime OrderDate { get; set; }
+
+        [Range(0.1,float.MaxValue)]
+        public float TotalPrice { get; set; }
+
+        [Display(Name="Username")]
+        public string UserID { get; set; }
+
+        public virtual User User { get; set; }
+
+        public virtual ICollection<OrderDetail> OrderDetails { get; set; }
+
+
+    }
+}
