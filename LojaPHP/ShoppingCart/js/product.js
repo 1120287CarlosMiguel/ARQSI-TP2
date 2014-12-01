56180b44d5df3177b873bade3c4da87a3f8e914a
tnmendes <tnmendes@tiagomendessmbp.home>
2014-12-01 21:08:36 +0000

    Shopping cart primeira fase
    neste momento o carrinho esta a funcionar mas sem aceder a uma BD

---
+++ LojaPHP/ShoppingCart/js/product.js
@@ -0,0 +1,24 @@
+//----------------------------------------------------------------
+// product class
+/*
+function product(sku, name, description, price, cal, carot, vitc, folate, potassium, fiber) {
+    this.sku = sku; // product code (SKU = stock keeping unit)
+    this.name = name;
+    this.description = description;
+    this.price = price;
+    this.cal = cal;
+    this.nutrients = {
+        "Carotenoid": carot,
+        "Vitamin C": vitc,
+        "Folates": folate,
+        "Potassium": potassium,
+        "Fiber": fiber
+    };
+*/
+function product(albumID, title, price, artist, genre) {
+    this.albumID = albumID;
+    this.title = title;
+    this.price = price;
+    this.artist = artist;
+    this.genre = genre;
+}
---
+++ LojaPHP/ShoppingCart/js/store.js
@@ -0,0 +1,40 @@
+﻿//----------------------------------------------------------------
+// store (contains the products)
+//
+// NOTE: nutritional info from http://www.cspinet.org/images/fruitcha.jpg
+// score legend:
+// 0: below 5% of daily value (DV)
+// 1: 5-10% DV
+// 2: 10-20% DV
+// 3: 20-40% DV
+// 4: above 40% DV
+//
+function store() {
+    this.products = [
+        new product("1", "Rock or Bust", 16, "AC DC", "Rock"),
+        new product("2", "Thriller", 8.44, "Michael Jackson", "Pop, rock, R&B"),
+        new product("3", "The Dark Side of the Moon", 11.99, "Pink Floyd", "Progressive rock"),
+        new product("4", "Their Greatest Hits (1971–1975)", 8.44, "Eagles", "Rock, soft rock, folk rock")
+    ];
+    this.dvaCaption = [
+        "Negligible",
+        "Low",
+        "Average",
+        "Good",
+        "Great"
+    ];
+    this.dvaRange = [
+        "below 5%",
+        "between 5 and 10%",
+        "between 10 and 20%",
+        "between 20 and 40%",
+        "above 40%"
+    ];
+}
+store.prototype.getProduct = function (albumID) {
+    for (var i = 0; i < this.products.length; i++) {
+        if (this.products[i].albumID == albumID)
+            return this.products[i];
+    }
+    return null;
+}
