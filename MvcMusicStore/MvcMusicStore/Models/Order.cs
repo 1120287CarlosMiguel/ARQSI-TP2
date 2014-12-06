9c7a4bbc1353b33a57be15a81dfad0b305b25c51
CapaoAzul <1110332@isep.ipp.pt>
2014-12-06 22:47:33 +0000

    Login check

---
+++ MvcMusicStore/MvcMusicStore/Models/Order.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace MvcMusicStore.Models
+{
+    public class Order
+    {
+        public int OrderID { get; set; }
+
+        [Required]
+        [StringLength(50, ErrorMessage = "First name cannot be longer than 50 characters.")]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(50, ErrorMessage = "Last name cannot be longer than 50 characters.")]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Order Date")]
+        public DateTime OrderDate { get; set; }
+
+        [Range(0.1, float.MaxValue)]
+        public float TotalPrice { get; set; }
+
+        public virtual ICollection<OrderDetail> OrderDetails { get; set; }
+
+    }
+}
---
+++ MvcMusicStore/MvcMusicStore/Models/OrderDetail.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace MvcMusicStore.Models
+{
+    public class OrderDetail
+    {
+        public int OrderDetailID { get; set; }
+
+        [Range(0,int.MaxValue)]
+        public int Quantity { get; set; }
+
+        [Range(0.0,float.MaxValue)]
+        public float Price { get; set; }
+
+        public int OrderID { get; set; }
+
+        public int AlbumID { get; set; }
+
+        public virtual Album Album { get; set; }
+
+        public virtual Order Order { get; set; }
+    }
+}
