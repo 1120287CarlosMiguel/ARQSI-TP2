f996c55a0341f7ecbac7c1bdbd50d4caa95f9be6
CapaoAzul <1110332@isep.ipp.pt>
2014-12-08 23:42:36 +0000

    Restrições a funcionar

--- MvcMusicStore/MvcMusicStore/MvcMusicStore/Controllers/StoreController.cs
+++ MvcMusicStore/MvcMusicStore/MvcMusicStore/Controllers/StoreController.cs
@@ -41,7 +41,7 @@ namespace MvcMusicStore.Controllers
         public ActionResult Browse(string genre)
         {
             // Retrieve Genre and its Associated Albums from database
-            var genreModel = this.storeDB.Genres.Include("Albums")
+            var genreModel = storeDB.Genres.Include("Albums")
           .Single(g => g.Name == genre);
 
             return this.View(genreModel);
